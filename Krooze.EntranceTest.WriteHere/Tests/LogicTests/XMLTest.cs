12a309e361cd8d600b2e366871cb85de4921bbcc
luispedrotrindade <lp_tp@hotmail.com>
2019-09-18 18:06:02 -0300

    Update XMLTest.cs

--- Krooze.EntranceTest.WriteHere/Tests/LogicTests/XMLTest.cs
+++ Krooze.EntranceTest.WriteHere/Tests/LogicTests/XMLTest.cs
@@ -1,4 +1,4 @@
-﻿using Krooze.EntranceTest.WriteHere.Structure.Model;
+using Krooze.EntranceTest.WriteHere.Structure.Model;
 using System.Data;
 using System.IO;
 using System.Reflection;
@@ -13,10 +13,15 @@ namespace Krooze.EntranceTest.WriteHere.Tests.LogicTests
         {
             //TODO: Take the Cruises.xml file, on the Resources folder, and translate it to the CruisesDTO object,
             //you can do it in any way, including intermediary objects
+
             XElement xmlFile = XElement.Load("Resources/Cruises.xml");
-            var serializer = new XmlSerializer(typeof(CruiseDTO));
-            
-            return (CruiseDTO)serializer.Deserialize(xmlFile.CreateReader());
+
+            CruiseDTO result = new CruiseDTO
+            {
+                CruiseCode = xmlFile.Element("ConfigCode").Value
+            };
+
+            return result;
         }
 
     }
