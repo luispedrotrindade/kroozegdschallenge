06b21a4c2d6d8524dd4070affa1c1df7fb9be919
luispedrotrindade <lp_tp@hotmail.com>
2019-09-19 08:08:55 -0700

    #

--- Krooze.EntranceTest.WriteHere/Tests/LogicTests/XMLTest.cs
+++ Krooze.EntranceTest.WriteHere/Tests/LogicTests/XMLTest.cs
@@ -1,9 +1,12 @@
 using Krooze.EntranceTest.WriteHere.Structure.Model;
+using System;
+using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Reflection;
 using System.Xml.Linq;
 using System.Xml.Serialization;
+using System.Linq;
 
 namespace Krooze.EntranceTest.WriteHere.Tests.LogicTests
 {
@@ -18,8 +21,31 @@ namespace Krooze.EntranceTest.WriteHere.Tests.LogicTests
 
             CruiseDTO result = new CruiseDTO
             {
-                CruiseCode = xmlFile.Element("ConfigCode").Value
+                CruiseCode = xmlFile.Element("CruiseId").Value,
+                TotalValue = Convert.ToDecimal(xmlFile.Element("TotalAllInclusiveCabinPrice").Value.Replace(".",",")),
+                CabinValue = Convert.ToDecimal(xmlFile.Element("CabinPrice").Value.Replace(".", ",")),
+                PortCharge = Convert.ToDecimal(xmlFile.Element("PortChargesAmt").Value.Replace(".", ",")),
+                ShipName = xmlFile.Element("ShipName").Value,
+                PassengerCruise = new List<PassengerCruiseDTO>()
             };
+
+            XElement categoryPrice = xmlFile.Element("CategoryPriceDetails");
+
+            IEnumerable<XElement> passengers = categoryPrice.Elements();
+
+
+            foreach (XElement passenger in passengers)
+            {
+                result.PassengerCruise.Add(new PassengerCruiseDTO
+                {
+                    Cruise = new CruiseDTO {
+                        CabinValue = Convert.ToDecimal(passenger.Elements("Charge").Where(x=>x.Attribute("ChargeType").Value == "CAB").First().Element("GrossAmount").Value.Replace(".", ",")),
+                        PortCharge = Convert.ToDecimal(passenger.Elements("Charge").Where(x=>x.Attribute("ChargeType").Value == "PCH").First().Element("NetAmount").Value.Replace(".", ",")),
+                        TotalValue = Convert.ToDecimal(passenger.Element("AllInclusivePerPax").Value.Replace(".", ","))                        
+                    },
+                    PassengerCode = passenger.FirstAttribute.Value
+                });
+            }
 
             return result;
         }
--- Krooze.EntranceTest.WriteHere/Tests/WebTests/WebTest.cs
+++ Krooze.EntranceTest.WriteHere/Tests/WebTests/WebTest.cs
@@ -1,4 +1,8 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
 
 namespace Krooze.EntranceTest.WriteHere.Tests.WebTests
 {
@@ -8,14 +12,72 @@ namespace Krooze.EntranceTest.WriteHere.Tests.WebTests
         {
             //TODO: Consume the following API: https://swapi.co/documentation using only .NET standard libraries (do not import the helpers on this page)
             // -Return the films object
-            return null;
+
+            string result = SubmitSwapiAPI("films", "GET", "application/json");
+
+            return JObject.Parse(result);
         }
 
         public string GetDirector()
         {
             //TODO: Consume the following API: https://swapi.co/documentation using only .NET standard libraries (do not import the helpers on this page)
             // -Return the name of person that directed the most star wars movies, based on the films object return
-            return null;
+            string result = SubmitSwapiAPI("films", "GET", "application/json");
+
+            JObject json = JObject.Parse(result);
+
+            string director = "";
+
+            Dictionary<string, int> directors = new Dictionary<string, int>();
+
+            directors.Add(json["results"][0]["director"].ToString(), 1);
+
+            foreach (JToken value in json["results"])
+            {
+                if (!directors.Keys.Any(x => x == value["director"].ToString()))
+                {
+                    directors.Add(value["director"].ToString(), 1);
+                }
+                else
+                {
+                    directors.FirstOrDefault(x => x.Key == value["director"].ToString()).Value = directors.FirstOrDefault(x => x.Key == value["director"].ToString()).Value + 1;
+                }
+            }
+
+
+            return directors.Where(x => x.Value == directors.Values.Max()).First().Key;
+        }
+
+
+
+        public static string SubmitSwapiAPI(string request, string httpMethod, string contentType)
+        {
+            string remoteUrl = "https://swapi.co/api/" + request;
+
+            HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(remoteUrl);
+
+            httpRequest.Method = httpMethod;
+            httpRequest.ContentType = contentType;
+
+
+            // Get the response from remote server
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpRequest.GetResponse();
+            Stream responseStream = httpWebResponse.GetResponseStream();
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            using (StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    sb.Append(line);
+                }
+            }
+
+            string serverResponse = sb.ToString();
+
+            return serverResponse;
+
         }
     }
 }
